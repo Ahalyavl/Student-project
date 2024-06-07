19b7f81beb154e63e5f86e6dbdf4951f60df5ddd
thenameisahalya <ahalyavl24@gmail.com>
2024-06-07 16:07:57 +0530

    project done

---
+++ src/components/ViewStudent.jsx
@@ -0,0 +1,161 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const ViewStudent = () => {
+    return (
+        <div>
+           <NavBar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" height="300px" class="card-img-top" alt="..."></img>
+                                        <div className="card-body">
+                                            <h5 className="card-title">dept-Chemistry</h5>
+                                            <p className="card-text">Ahalya</p>
+                                            <a href="#" className="btn btn-primary">Roll No.11</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" height="300px" class="card-img-top" alt="...">
+                                       </img> <div className="card-body">
+                                            <h5 className="card-title">dept-history</h5>
+                                            <p className="card-text">Noah</p>
+                                            <a href="#" className="btn btn-primary">Roll N0.12</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card" >
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" height="300px" class="card-img-top" alt="...">
+                                        </img><div className="card-body">
+                                            <h5 className="card-title">dept-chemistry</h5>
+                                            <p className="card-text">Ria</p>
+                                            <a href="#" className="btn btn-primary">Roll No.13</a>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" height="300px" class="card-img-top" alt="...">
+                                      </img>  <div className="card-body">
+                                            <h5 className="card-title">dept-chemistry</h5>
+                                            <p className="card-text">Luna</p>
+                                            <a href="#" className="btn btn-primary">Roll No.14</a>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card" >
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" height="300px" class="card-img-top" alt="...">
+                                       </img> <div className="card-body">
+                                            <h5 className="card-title">dept-chemistry</h5>
+                                            <p className="card-text">Aadi</p>
+                                            <a href="#" className="btn btn-primary">Roll No.15</a>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" height="300px" class="card-img-top" alt="...">
+                                       </img> <div className="card-body">
+                                            <h5 className="card-title">dept-chemistry</h5>
+                                            <p className="card-text">Lexi</p>
+                                            <a href="#" className="btn btn-primary">Roll No.16</a>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" height="300px" class="card-img-top" alt="...">
+                                      </img>  <div className="card-body">
+                                            <h5 className="card-title">dept-chemistry</h5>
+                                            <p className="card-text">Reena</p>
+                                            <a href="#" className="btn btn-primary">Roll No.17</a>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" height="300px" class="card-img-top" alt="...">
+                                       </img> <div className="card-body">
+                                            <h5 className="card-title">dept-chemistry</h5>
+                                            <p className="card-text">Miya</p>
+                                            <a href="#" className="btn btn-primary"></a>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" height="300px" class="card-img-top" alt="...">
+                                        </img><div className="card-body">
+                                            <h5 className="card-title">dept-chemistry</h5>
+                                            <p className="card-text">Jiya</p>
+                                            <a href="#" className="btn btn-primary">Roll no.20</a>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card" >
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" height="300px" class="card-img-top" alt="...">
+                                      </img>  <div className="card-body">
+                                            <h5 className="card-title">dept-chemistry</h5>
+                                            <p className="card-text">Arya</p>
+                                            <a href="#" className="btn btn-primary">Roll no.32</a>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" height="300px" class="card-img-top" alt="...">
+                                       </img> <div class="card-body">
+                                            <h5 class="card-title">dept-chemistry</h5>
+                                            <p class="card-text"></p>
+                                            <a href="#" class="btn btn-primary">Roll no.34</a>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" height="300px" class="card-img-top" alt="...">
+                                       </img> <div className="card-body">
+                                            <h5 className="card-title">dept-chemistry</h5>
+                                            <p className="card-text">Mike</p>
+                                            <a href="#" className="btn btn-primary">roll no.56</a>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewStudent
