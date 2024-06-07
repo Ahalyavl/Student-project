deec0d67ce7c7ce9bb30977c7b832abf856200fc
thenameisahalya <ahalyavl24@gmail.com>
2024-06-07 20:45:28 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import AddStudent from './components/AddStudent';
 import SearchStudent from './components/SearchStudent';
 import DeleteStudent from './components/DeleteStudent';
 import ViewStudent from './components/ViewStudent';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div >
-     <AddStudent/>
-     <SearchStudent/>
-     <DeleteStudent/>
-     <ViewStudent/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<AddStudent/>}/>
+      <Route path='/search' element={<SearchStudent/>}/>
+      <Route path='/delete' element={<DeleteStudent/>}/>
+      <Route path='/ViewStudent' element={<ViewStudent/>}/>
+    </Routes>
+    </BrowserRouter>
      
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="AddStudent.jsx">Student</a>
+    <Link class="navbar-brand" to="#">Student</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="SearchStudent.jsx">Search</a>
+          <Link class="nav-link active" aria-current="page" to ="/search">Search</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="DeleteStudent.jsx">Delete</a>
+          <Link class="nav-link" to="/delete">Delete</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="ViewAll.jsx">ViewAll</a>
+          <Link class="nav-link" to="/ViewStudent">ViewStudent</Link>
         </li>
       </ul>
      
